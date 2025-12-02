e27fa4f5c5cbbc6a1649b9c73b69c8144230f9ab
benresonance-star <benresonance@gmail.com>
2025-12-02 18:21:43 +1100

    Add makeId function for generating unique IDs
    Added a simple ID generator function to create unique IDs.

--- js/utils.js
+++ js/utils.js
@@ -14,3 +14,25 @@ function el(type, className, fill) {
   if (fill) e.innerHTML = fill;
   return e;
 }
+
+// Small helpers living here
+
+function qs(sel) {
+  return document.querySelector(sel);
+}
+
+function qsa(sel) {
+  return Array.from(document.querySelectorAll(sel));
+}
+
+function el(type, className, fill) {
+  const e = document.createElement(type);
+  if (className) e.className = className;
+  if (fill) e.innerHTML = fill;
+  return e;
+}
+
+// Simple id generator for new sections/subsections/tasks
+function makeId(prefix) {
+  return prefix + "-" + Math.random().toString(36).slice(2, 10);
+}
